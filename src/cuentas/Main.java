f46bd8c1ce93c4b38906aa81ebff66242faf4647
valla <valla@DEIVIS-PC>
2025-02-15 01:38:42 +0000

    Agregando las dos clases principales

---
+++ src/cuentas/Main.java
@@ -0,0 +1,26 @@
+package cuentas;
+
+
+import cuentas.CCuenta;
+
+public class Main {
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+
+        operaciones(cuenta1);
+
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+    }
+
+    private static void operaciones(CCuenta cuenta1) {
+        // Operaciones de ingreso y retiro
+        cuenta1.retirar(2300);
+        cuenta1.ingresar(685);
+    }
+}
